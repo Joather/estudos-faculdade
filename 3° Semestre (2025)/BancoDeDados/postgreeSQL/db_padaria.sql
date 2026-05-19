677862d9fafebc114a6b64e194369649c5fa4341
¨Joather¨ <joather.jvaf@gmail.com>
2026-05-19 02:58:26 -0300

    Exemplo de Banco de Dados em PostgreeSQL + organização de pastas de outras matérias

--- 3° Semestre (2025)/BancoDeDados/postgreeSQL/db_padaria.sql
+++ 3° Semestre (2025)/BancoDeDados/postgreeSQL/db_padaria.sql
@@ -1,3 +1,4 @@
+DROP TABLE produtos;
 CREATE TABLE produtos (
 	id INT PRIMARY KEY,
 	nome VARCHAR(255),
@@ -6,7 +7,28 @@ CREATE TABLE produtos (
 INSERT INTO produtos (id, nome, preco)
 VALUES
 	(01, 'pao_frances', 0.25),
-	(02, 'caixa_leite', 6.00),
+	(02, 'caixa_de_leite', 6.00),
 	(03, 'pao_de_queijo', 0.50);
+
+ALTER TABLE produtos
+ADD promocao INT;
+
+UPDATE produtos
+SET nome = 'pao_dormindo', preco = 0.10, promocao = 60
+WHERE id = 01;
+
+ALTER TABLE produtos
+ALTER COLUMN promocao TYPE VARCHAR(3);
+
+ALTER TABLE produtos
+ALTER COLUMN nome TYPE VARCHAR(50);
+
+ALTER TABLE produtos
+DROP COLUMN promocao;
+
+DELETE FROM produtos
+WHERE nome = 'pao_dormindo';
+
+TRUNCATE TABLE produtos;
 
 SELECT * FROM produtos;
