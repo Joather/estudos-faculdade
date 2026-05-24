9a5e3c73e83f18095420dde25797fad00693e261
¨Joather¨ <joather.jvaf@gmail.com>
2026-05-24 19:14:54 -0300

    up

---
+++ 3-semestre/codigo-fodase/TiposDeVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposDeVariaveis {
+
+    public static void main(String[] args) {
+        // Criando as TiposDeVariaveis
+        int itens = 50;
+        float precoPorItem = 9.99f;
+        float custoTotal = itens * precoPorItem;
+        char moeda = '$';
+
+        // Imprimindo as TiposDeVariaveis
+        System.out.println("Quantidade de itens: " + itens);
+        System.out.println("Preço por item: " + precoPorItem + moeda);
+        System.out.println("Custo total: " + custoTotal);
+    }
+}
